00f1fe012ab26532c43976b7836f659d85943747
bipuldey19 <bipuldey19@gmail.com>
2024-01-06 20:29:14 +0600

    Added /

--- server.js
+++ server.js
@@ -1,77 +1,115 @@
-const axios = require('axios');
-const express = require('express');
+const axios = require("axios");
+const express = require("express");
 
 const app = express();
 const port = 3000;
 
-app.get('/ott', async (req, res) => {
-    try {
-        let toffeeResponse = await axios.get('https://raw.githubusercontent.com/Jeshan-akand/Toffee-Channels-Link-Headers/main/toffee_channel_data.json');
-        let toffeeChannelData = toffeeResponse.data.channels;
-
-        let tsportsResponse = await axios.get('https://raw.githubusercontent.com/byte-capsule/TSports-m3u8-Grabber/main/TSports_m3u8_headers.Json');
-        let tsportsChannelData = tsportsResponse.data.channels;
-
-        let combinedChannelData = [...toffeeChannelData, ...tsportsChannelData];
-
-        combinedChannelData.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
-
-        let formattedChannels = '';
-
-        for (let i = 0; i < combinedChannelData.length; i++) {
-            const channel = combinedChannelData[i];
-
-            formattedChannels += `#EXTINF:-1 tvg-chno="${i + 1}" tvg-id="" tvg-logo="${channel.logo}", ${channel.name}\n`;
-            formattedChannels += `#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.193 Mobile Safari/537.36\n`;
-
-            const cookieProperty = 'cookie' in channel.headers ? 'cookie' : 'Cookie';
-            formattedChannels += `#EXTHTTP:${JSON.stringify({ [cookieProperty]: channel.headers[cookieProperty] })}\n`;
-
-            formattedChannels += `${channel.link}\n`;
-        }
-
-        res.send(formattedChannels);
-    } catch (error) {
-        console.error('Error fetching API:', error);
-        res.status(500).send('Internal Server Error');
-    }
+app.get("/", (req, res) => {
+  res.json({
+    name: "Toffee App + Tsports All Channel m3u link for Android",
+    apps: [
+      {
+        name: "OTT Navigator",
+        link:
+          "/ott",
+      },
+      {
+        name: "NS Player",
+        link:
+          "/ns",
+      },
+    ],
+  });
 });
 
-app.get('/ns', async (req, res) => {
+app.get("/ott", async (req, res) => {
   try {
-      let toffeeResponse = await axios.get('https://raw.githubusercontent.com/Jeshan-akand/Toffee-Channels-Link-Headers/main/toffee_channel_data.json');
-      let toffeeChannelData = toffeeResponse.data.channels;
+    let toffeeResponse = await axios.get(
+      "https://raw.githubusercontent.com/Jeshan-akand/Toffee-Channels-Link-Headers/main/toffee_channel_data.json"
+    );
+    let toffeeChannelData = toffeeResponse.data.channels;
 
-      let tsportsResponse = await axios.get('https://raw.githubusercontent.com/byte-capsule/TSports-m3u8-Grabber/main/TSports_m3u8_headers.Json');
-      let tsportsChannelData = tsportsResponse.data.channels;
+    let tsportsResponse = await axios.get(
+      "https://raw.githubusercontent.com/byte-capsule/TSports-m3u8-Grabber/main/TSports_m3u8_headers.Json"
+    );
+    let tsportsChannelData = tsportsResponse.data.channels;
 
-      let combinedChannelData = [...toffeeChannelData, ...tsportsChannelData];
+    let combinedChannelData = [...toffeeChannelData, ...tsportsChannelData];
 
-      combinedChannelData.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+    combinedChannelData.sort((a, b) =>
+      a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+    );
 
-      let formattedChannels = [];
+    let formattedChannels = "";
 
-      for (let i = 0; i < combinedChannelData.length; i++) {
-        const channel = combinedChannelData[i];
-        const cookieProperty = 'cookie' in channel.headers ? 'cookie' : 'Cookie';
+    for (let i = 0; i < combinedChannelData.length; i++) {
+      const channel = combinedChannelData[i];
 
-          formattedChannels.push({
-              name: channel.name,
-              link: channel.link,
-              logo: channel.logo,
-              origin: channel.link.substring(0, channel.link.indexOf('/', channel.link.indexOf('//') + 2)),
-              userAgent: 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.193 Mobile Safari/537.36',
-              cookie: channel.headers[cookieProperty]
-          });
-      }
+      formattedChannels += `#EXTINF:-1 tvg-chno="${
+        i + 1
+      }" tvg-id="" tvg-logo="${channel.logo}", ${channel.name}\n`;
+      formattedChannels += `#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.193 Mobile Safari/537.36\n`;
 
-      res.json(formattedChannels);
+      const cookieProperty = "cookie" in channel.headers ? "cookie" : "Cookie";
+      formattedChannels += `#EXTHTTP:${JSON.stringify({
+        [cookieProperty]: channel.headers[cookieProperty],
+      })}\n`;
+
+      formattedChannels += `${channel.link}\n`;
+    }
+
+    res.send(formattedChannels);
   } catch (error) {
-      console.error('Error fetching API:', error);
-      res.status(500).send('Internal Server Error');
+    console.error("Error fetching API:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/ns", async (req, res) => {
+  try {
+    let toffeeResponse = await axios.get(
+      "https://raw.githubusercontent.com/Jeshan-akand/Toffee-Channels-Link-Headers/main/toffee_channel_data.json"
+    );
+    let toffeeChannelData = toffeeResponse.data.channels;
+
+    let tsportsResponse = await axios.get(
+      "https://raw.githubusercontent.com/byte-capsule/TSports-m3u8-Grabber/main/TSports_m3u8_headers.Json"
+    );
+    let tsportsChannelData = tsportsResponse.data.channels;
+
+    let combinedChannelData = [...toffeeChannelData, ...tsportsChannelData];
+
+    combinedChannelData.sort((a, b) =>
+      a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+    );
+
+    let formattedChannels = [];
+
+    for (let i = 0; i < combinedChannelData.length; i++) {
+      const channel = combinedChannelData[i];
+      const cookieProperty = "cookie" in channel.headers ? "cookie" : "Cookie";
+
+      formattedChannels.push({
+        name: channel.name,
+        link: channel.link,
+        logo: channel.logo,
+        origin: channel.link.substring(
+          0,
+          channel.link.indexOf("/", channel.link.indexOf("//") + 2)
+        ),
+        userAgent:
+          "Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.193 Mobile Safari/537.36",
+        cookie: channel.headers[cookieProperty],
+      });
+    }
+
+    res.json(formattedChannels);
+  } catch (error) {
+    console.error("Error fetching API:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
